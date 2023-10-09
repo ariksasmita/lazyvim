6b83522db870a60675e73c51496a8866e8cabb51
Arik Sasmita <ariksasmita@gmail.com>
2023-10-09 08:08:42 +0800

    Create setup.lua

---
+++ lua/plugins/setup.lua
@@ -0,0 +1,12 @@
+return {
+  -- add moonfly
+  { "bluz71/vim-moonfly-colors" },
+
+  -- Config Lazyvim to load moonfly
+  {
+    "LazyVim/LaxyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+}
